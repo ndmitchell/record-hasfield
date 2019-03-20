c3335cc4755927370db7832c8741bea53b41c049
Neil Mitchell <ndmitchell@gmail.com>
2019-03-20 13:27:11 +0000

    Initial version

---
+++ src/GHC/Records/Future.hs
@@ -0,0 +1,31 @@
+{-# LANGUAGE AllowAmbiguousTypes
+           , FunctionalDependencies
+           , KindSignatures
+           , ScopedTypeVariables
+           , TypeApplications
+  #-}
+
+-- | This module provides a version of "GHC.Records" as it will be after the implementation of
+--   <https://github.com/ghc-proposals/ghc-proposals/blob/master/proposals/0042-record-set-field.rst GHC proposal #42>.
+module GHC.Records.Future
+    ( HasField(..)
+    , getField
+    , setField
+    ) where
+
+-- | Constraint representing the fact that the field @x@ can be get and set on
+--   the record type @r@ and has field type @a@.  This constraint will be solved
+--   automatically, but manual instances may be provided as well.
+--
+--   The function should satisfy the invariant:
+--
+-- > uncurry ($) (hasField @x r) == r
+class HasField x r a | x r -> a where
+    -- | Function to get and set a field in a record.
+    hasField :: r -> (a -> r, a)
+
+getField :: forall x r a . HasField x r a => r -> a
+getField = snd . hasField @x
+
+setField :: forall x r a . HasField x r a => r -> a -> r
+setField = fst . hasField @x
